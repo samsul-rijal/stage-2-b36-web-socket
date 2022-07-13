daed87803d7827875c1d01110d397e9aa3f82502
Your Name <samsulr081@gmail.com>
2022-07-13 11:43:12 +0700

    update

--- client/src/pages/Complain.js
+++ client/src/pages/Complain.js
@@ -2,23 +2,43 @@
 import React, { useEffect } from 'react'
 
 import Navbar from '../components/Navbar'
+import { Container, Row, Col } from 'react-bootstrap'
 
 // import components here
+import Contact from '../components/complain/Contact'
 
 // import socket.io-client 
 import {io} from 'socket.io-client'
+import { useState } from 'react'
 
 // initial variable outside component
 let socket
 export default function Complain() {
-    // code here
-
     const title = "Complain"
     document.title = 'DumbMerch | ' + title
+
+    // code here
+    const [contact, setContact] = useState(null)
+    const [contacts, setContacts] = useState([])
+
+    const loadContact = () => {
+        socket.emit("load admin contact")
+
+        socket.on("admin contact", (data) => {
+            console.log(data);
+
+            const dataContact = {
+                ...data,
+                message: "Click here to start message"
+            }
+            setContacts([dataContact])
+        })
+    }
 
     useEffect(() =>{
         socket = io('http://localhost:5000')
         // code here
+        loadContact()
 
         return () => {
             socket.disconnect()
@@ -26,11 +46,22 @@ export default function Complain() {
     }, [])
 
     // code here
+    const onClickContact = (data) => {
+        console.log(data);
+        setContact(data)
+    }
     
     return (
         <>
             <Navbar title={title} />
             {/* code here */}
+            <Container>
+                <Row>
+                    <Col>
+                        <Contact dataContact={contacts} clickContact={onClickContact} contact={contact} />
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
--- client/src/pages/ComplainAdmin.js
+++ client/src/pages/ComplainAdmin.js
@@ -1,9 +1,11 @@
 // import hook
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import NavbarAdmin from '../components/NavbarAdmin'
+import { Container, Row, Col } from 'react-bootstrap'
 
 // import components here
+import Contact from '../components/complain/Contact'
 
 // import socket.io-client 
 import {io} from 'socket.io-client'
@@ -11,14 +13,36 @@ import {io} from 'socket.io-client'
 // initial variable outside socket
 let socket
 export default function ComplainAdmin() {
-    // code here
-
     const title = "Complain admin"
     document.title = 'DumbMerch | ' + title
+    
+    // code here
+    const [contact, setContact] = useState(null)
+    const [contacts, setContacts] = useState([])
+
+    const loadContact = () => {
+        socket.emit("load custommer contacts")
+
+        socket.on("custommer contacts", (data) => {
+            console.log(data);
+
+            
+            let dataContacts = data.filter(item => item.status !== 'admin')
+
+            dataContacts = dataContacts.map((item) => ({
+                ...item,
+                message: "Click here to start message"
+            }))
+
+            setContacts(dataContacts)
+            console.log(dataContacts);
+        })
+    }
 
     useEffect(() =>{
         socket = io('http://localhost:5000')
         // code here
+        loadContact()
 
         return () => {
             socket.disconnect()
@@ -26,11 +50,23 @@ export default function ComplainAdmin() {
     }, [])
 
     // code here
+    const onClickContact = (data) => {
+        console.log(data);
+        setContact(data)
+    }
 
     return (
         <>
             <NavbarAdmin title={title} />
             {/* code here */}
+
+            <Container>
+                <Row>
+                    <Col>
+                        <Contact dataContact={contacts} clickContact={onClickContact} contact={contact} />
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
